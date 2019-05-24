93285891b0d2c428347d4a599ac3c28c56be9e8e
yuwzhang <yuwzhang@cisco.com>
2019-05-24 17:40:47 +0800

    : feat(python_data_structure-1): add the uses of heap

--- python_data_structure-1.py
+++ python_data_structure-1.py
@@ -44,7 +44,7 @@ def assign_variable1():
         elif tag == 'bar':
             do_bar(*args)
 
-# Question: 保留最后 N 个元素
+# Question3: 保留最后 N 个元素
 # Answer: 使用 deque 数据结构
 
 
@@ -69,10 +69,50 @@ def record_n_elements_of_files():
             print('-' * 20)
 
 
+# Question4: 查找最大或者最小的 N 个元素
+# Answer:  使用堆队列 heapq 实现，原因是由于堆数据结构的特征，堆顶总是最大（或者最小）
+# 的元素，同时堆顶也就表示着优先级最高的元素。进而每次取堆顶的值，就是我们题目中所要求的。
+
+
+def find_the_largest_or_smallest_n_elements():
+    import heapq
+    # simple list
+    nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+    print(heapq.nlargest(3, nums))
+    print(heapq.nsmallest(3, nums))
+
+    # complicated data structure
+    portfolio = [
+        {'name': 'IBM', 'shares': 100, 'price': 91.1},
+        {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+        {'name': 'FB', 'shares': 200, 'price': 21.09},
+        {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+        {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+        {'name': 'ACME', 'shares': 75, 'price': 115.65}
+    ]
+    cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+    expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+    print(cheap)
+    print(expensive)
+
+    # In each operation, get the result with the highest priority value
+    nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+    heap = list(nums)
+    heapq.heapify(heap)
+    # get the -4, default is min-heap
+    print(heapq.heappop(heap))
+    print(heap)
+    # get the 1
+    print(heapq.heappop(heap))
+    print(heap)
+
+
 if __name__ == '__main__':
     # assign_variable()
 
     # assign_variable1()
 
-    record_n_elements_of_files()
+    # record_n_elements_of_files()
+
+    find_the_largest_or_smallest_n_elements()
 
