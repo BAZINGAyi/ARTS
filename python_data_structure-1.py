cc285610927065048e38eab5045f074b72106e5c
yuwzhang <yuwzhang@cisco.com>
2019-05-27 17:28:13 +0800

    : feat(python_data_structure-1): add the uses of multi dict and ordered dict

--- python_data_structure-1.py
+++ python_data_structure-1.py
@@ -1,5 +1,6 @@
 # Queston1: 现在有一个包含 N 个元素的元组或者是序列，怎样将它里面的值解压后同时赋值给 N 个变量？
 # Answer: 这种解压方式适用于 元组，序列，字符串，文件对象，迭代器和生成器等确定长度的迭代对象
+from collections import OrderedDict
 
 
 def assign_variable():
@@ -149,7 +150,59 @@ def test_priority_queue():
     print(q.pop())
 
 
+# Question6: 怎样实现一个键对应多个值的字典（也叫 multidict）？
+# Answer: 将多个值放入 list 或者 集合中, 或者使用 defaultdict 的数据结构
 
+def multidict_dict():
+    from collections import defaultdict
+
+    multidict_list = {
+        'a': [1, 2, 3],
+        'b': [4, 5]
+    }
+
+    multidict_set = {
+        'a': {1, 2, 3},
+        'b': {4, 5}
+    }
+
+    d = defaultdict(list)
+    d['a'].append(1)
+    d['a'].append(2)
+    d['b'].append(4)
+
+    d = defaultdict(set)
+    d['a'].add(1)
+    d['a'].add(2)
+    d['b'].add(4)
+
+    # defaultdict 会自动为将要访问的键（就算目前字典中并不存在这样的键）创建映射实体。
+    #  如果你并不需要这样的特性，在一个普通的字典上使用 setdefault() 方法来代替。
+
+    d = {}
+    d.setdefault('a', []).append(1)
+    d.setdefault('a', []).append(2)
+    d.setdefault('b', []).append(4)
+
+
+# Question7:  你想创建一个字典，并且在迭代或序列化这个字典的时候能够控制元素的顺序。
+# Answer: 可以用 collections 中的 OrderedDict 类
+
+
+def deal_ordered_dic():
+    # 注意一个 OrderedDict 的大小是普通字典的两倍，因为内部维护着一个链表，在读取大量的
+    # 数据时，需要权衡一下内存的印象。
+    d = OrderedDict()
+    d['foo'] = 1
+    d['bar'] = 2
+    d['spam'] = 3
+    d['grok'] = 4
+
+    for key in d:
+        print(key, d[key])
+
+    import json
+    print(json.dumps(d))
 
 
 if __name__ == '__main__':
@@ -161,6 +214,10 @@ if __name__ == '__main__':
 
     # find_the_largest_or_smallest_n_elements()
 
-    test_priority_queue()
+    # test_priority_queue()
+
+    # multidict_dict()
+
+    deal_ordered_dic()
 
 
