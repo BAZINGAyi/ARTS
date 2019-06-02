99ef84694f91cb41f143f378ffda4bce12731cb0
gus <potflesh@gmail.com>
2019-06-02 16:49:12 +0800

    fixed(python_data_structure1): add the ability to extract sub dict from dict

--- python_data_structure-1.py
+++ python_data_structure-1.py
@@ -516,6 +516,27 @@ def filter_sequence_elements():
     # ['5800 E 58TH', '1060 W ADDISON', '4801 N BROADWAY']
 
 
+# Question17: How to extract sub-dictionary from dictionary
+# Answer: Use dictionary comprehensions
+def extract_sub_dic_from_dic():
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+    # Make a dictionary of all prices over 200
+    p1 = {key: value for key, value in prices.items() if value > 200}
+    # Make a dictionary of tech stocks
+    tech_names = {'AAPL', 'IBM', 'HPQ', 'MSFT'}
+    p2 = {key: value for key, value in prices.items() if key in tech_names}
+
+    # or use dict(), but it run more slowly than Using dictionary comprehensions
+    p1 = dict((key, value) for key, value in prices.items() if value > 200)
+    # Make a dictionary of tech stocks
+    tech_names = {'AAPL', 'IBM', 'HPQ', 'MSFT'}
+    p2 = {key: prices[key] for key in prices.keys() & tech_names}
 
 
 if __name__ == '__main__':
@@ -547,8 +568,9 @@ if __name__ == '__main__':
 
     # group_by_function()
 
-    filter_sequence_elements()
+    # filter_sequence_elements()
 
+    extract_sub_dic_from_dic()
 
 
 
