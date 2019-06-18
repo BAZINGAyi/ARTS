702eec62291110d8d07b97ed98918b2315b4d6a5
michael <potflesh@gmail.com>
2019-06-18 10:16:05 +0800

    feat(number_and_date): format or print the number()

--- python_number_and_date-1.py
+++ python_number_and_date-1.py
@@ -1,5 +1,3 @@
-
-
 # Question1: You want to perform a rounding operation with a specified
 # precision on a floating point number.
 def rounding_operation_in_floating_point_number():
@@ -48,7 +46,7 @@ def perform_precise_floating_point_arithmetic():
     from decimal import Decimal
     a = Decimal('4.2')
     b = Decimal('2.1')
-    print((a+b) == Decimal('6.3'))
+    print((a + b) == Decimal('6.3'))
 
     # One of the main features of the decimal module is that it allows you to
     # control every aspect of your calculations
@@ -77,7 +75,7 @@ def perform_precise_floating_point_arithmetic():
 def print_number_of_format():
     x = 1234.56789
     # Two decimal places of accuracy
-    print( format(x, '0.2f'))
+    print(format(x, '0.2f'))
     # Right justified in 10 chars, one-digit accuracy
     print(format(x, '>10.1f'))
     # Left justified
@@ -111,9 +109,45 @@ def print_number_of_format():
     print('%-10.1f' % x)
 
 
+# Question3: You need to format or print the Integer in binary,
+# octal, or hexadecimal
+def format_or_print_the_number():
+    x = 1234
+    print(bin(x))
+    print(oct(x))
+    print(hex(x))
+    print(format(x, 'b'))
+    print(format(x, 'o'))
+    print(format(x, 'x'))
+    # if you don't want to print the '0b' or '0o' or '0x', you can use the
+    # format()
+    print(format(x, 'b'))
+    print(format(x, 'o'))
+    print(format(x, 'x'))
+    # If the integers are signed, so If you are dealing with negative numbers,
+    # the output will contain a negative sign.
+    x = -1234
+    print(format(x, 'b'))
+    print(format(x, 'x'))
+    # If you want to produce a unsigned value, you need to add a indicate to
+    # represents the value of the most length
+    x = -1234
+    print(format(2**32 + x, 'b'))
+    print(format(2**32 + x, 'b'))
+    print(format(2**32 + x, 'x'))
+    # transfer string to int
+    print(int('4d2', 16))
+    print(int('10011010010', 2))
+    # But there are special in octal look like this:
+    import os
+    os.chmod('.test', 0o755)
+
+
 if __name__ == '__main__':
     # rounding_operation_in_floating_point_number()
 
     # perform_precise_floating_point_arithmetic()
 
-    print_number_of_format()
+    # print_number_of_format()
+
+    format_or_print_the_number()
