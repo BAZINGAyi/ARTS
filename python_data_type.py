994ad849f9a161ebcb4124cd86b0f2763df81f50
michael <potflesh@gmail.com>
2019-06-18 18:33:59 +0800

    feat(data_type): add the application for list_comprehension()

--- python_data_type.py
+++ python_data_type.py
@@ -118,6 +118,27 @@ def list_comprehension():
     result = num_list(num)
     print(result)
 
+    # 让所有奇数都在偶数前面，而且奇数升序排列，偶数降序排序，如字符串'1982376455',
+    # 变成'1355798642'
+
+    # 方法一
+    def func1(l):
+        if isinstance(l, str):
+            l = [int(i) for i in l]
+        l.sort(reverse=True)
+        for i in range(len(l)):
+            if l[i] % 2 > 0:
+                l.insert(0, l.pop(i))
+        print(''.join(str(e) for e in l))
+
+    # 方法二
+    def func2(l):
+        print("".join(
+            sorted(l, key=lambda x: int(x) % 2 == 0 and 20 - int(x) or int(x))))
+
+    func1('1982376455')
+    func2('1982376455')
+
 
 def merge_list():
     # To merge these two lists, you cannot use extend.
@@ -178,5 +199,6 @@ if __name__ == '__main__':
     # delete_list()
 
     # list_math_calculate()
+    list_comprehension()
 
-    get_missing_letter()
+    # get_missing_letter()
