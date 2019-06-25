7877e2fbf388e5e0795fd65c2b4ba0b21a682d4d
michael <potflesh@gmail.com>
2019-06-25 13:18:46 +0800

    feat(python_grammar): implements a iterator protocol

--- python_grammar1.py
+++ python_grammar1.py
@@ -135,6 +135,107 @@ def create_iterator_by_using_generator():
     # print(next(c))
 
 
+# Question4: Implement an iterator protocol
+def implement_an_iterator_protocol():
+    # So far, there is most easiest way to implement an iterator is By Using
+    # the generator
+    class Node:
+        def __init__(self, value):
+            self._value = value
+            self._children = []
+
+        def __repr__(self):
+            return 'Node({!r})'.format(self._value)
+
+        def add_child(self, node):
+            self._children.append(node)
+
+        def __iter__(self):
+            return iter(self._children)
+
+        def depth_first(self):
+            yield self
+            for c in self:
+                yield from c.depth_first()
+
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    for ch in root.depth_first():
+        print(ch)
+    # Outputs Node(0), Node(1), Node(3), Node(4), Node(2), Node(5)
+
+    # The iterator protocol of python requires __iter()__ return a special
+    # iterator object. This iterator object implements the __next__() method and
+    # identifies the completion of the iteration through the StopIteration
+    # exception. However, implementing these is often cumbersome.
+    class DepthFirstIterator(object):
+        '''
+        Depth-first traversal
+        '''
+
+        def __init__(self, start_node):
+            self._node = start_node
+            self._children_iter = None
+            self._child_iter = None
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            # Return myself if just started; create an iterator for children
+            if self._children_iter is None:
+                self._children_iter = iter(self._node)
+                return self._node
+            # If processing a child, return its next item
+            elif self._child_iter:
+                try:
+                    nextchild = next(self._child_iter)
+                    return nextchild
+                except StopIteration:
+                    self._child_iter = None
+                    return next(self)
+            # Advance to the next child and start its iteration
+            else:
+                self._child_iter = next(self._children_iter).depth_first()
+                return next(self)
+
+    class Node2:
+        def __init__(self, value):
+            self._value = value
+            self._children = []
+
+        def __repr__(self):
+            return 'Node({!r})'.format(self._value)
+
+        def add_child(self, node):
+            self._children.append(node)
+
+        def __iter__(self):
+            return iter(self._children)
+
+        def depth_first(self):
+            return DepthFirstIterator(self)
+
+    root = Node2(0)
+    child1 = Node2(1)
+    child2 = Node2(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node2(3))
+    child1.add_child(Node2(4))
+    child2.add_child(Node2(5))
+
+    for ch in root.depth_first():
+        print(ch)
+
+
 if __name__ == '__main__':
     # uses_asterisk()
 
@@ -142,4 +243,5 @@ if __name__ == '__main__':
 
     # manual_traversal_iterator()
     # proxy_iteration()
-    create_iterator_by_using_generator()
+    # create_iterator_by_using_generator()
+    implement_an_iterator_protocol()
