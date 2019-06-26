5d496a9f040d1c9c01c74c570313c5b0ab7f51f3
michael <potflesh@gmail.com>
2019-06-26 10:27:33 +0800

    feat(python_object_oriented): support many operators

--- python_object_oriented-1.py
+++ python_object_oriented-1.py
@@ -21,5 +21,38 @@ def traverse_all_property_in_class():
     print(dir(Bmw))  # 获Bmw所有的属性和方法\
 
 
+# Question2: Write a class and let it support as many operators as possible?
+def support_many_operators():
+    class Array:
+        __list = []
+
+        def __init__(self):
+            print("constructor")
+
+        def __del__(self):
+            print("destruct")
+
+        def __str__(self):
+            return "this self-defined array class"
+
+        def __getitem__(self, key):
+            return self.__list[key]
+
+        def __len__(self):
+            return len(self.__list)
+
+        def Add(self, value):
+            self.__list.append(value)
+
+        def Remove(self, index):
+            del self.__list[index]
+
+        def DisplayItems(self):
+            print("show all items---")
+            for item in self.__list:
+                print(item)
+
+
 if __name__ == '__main__':
-    traverse_all_property_in_class()
+    # traverse_all_property_in_class()
+    support_many_operators()
