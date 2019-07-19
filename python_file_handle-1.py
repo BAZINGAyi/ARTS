726bce7072040f3a3b53054921e36f7a96a81a5d
michael <potflesh@gmail.com>
2019-07-19 16:51:16 +0800

    feat(python_file): StudyInputOrOutput()

--- python_file_handle-1.py
+++ python_file_handle-1.py
@@ -1002,6 +1002,45 @@ def serializing_python_objects():
     # https://docs.python.org/3/library/pickle.html
 
 
+class StudyInputOrOutput():
+    def basic_input_or_output(self):
+        name = input('your name:')  # 等待键盘输入，知道回车被按下
+        gender = input('you are a boy?(y/n)')
+
+        ###### 输入 ######
+        # your name: Jack
+        # you are a boy?
+
+        welcome_str = 'Welcome to the matrix {prefix} {name}.'
+        welcome_dic = {
+            'prefix': 'Mr.' if gender == 'y' else 'Mrs',
+            'name': name
+        }
+
+        print('authorizing...')
+        print(welcome_str.format(**welcome_dic))
+
+        ########## 输出 ##########
+        # authorizing...
+        # Welcome to the matrix Mr.Jack.
+
+        a = input()
+        # 1
+        b = input()
+        # 2
+
+        print('a + b = {}'.format(a + b))
+        ########## 输出 ##############
+        # a + b = 12
+        # print('type of a is {}, type of b is {}'.format(type(a), type(b)))
+        ########## 输出 ##############
+        # type of a is <class 'str'>, type of b is < class 'str' >
+
+        print('a + b = {}'.format(int(a) + int(b)))
+        ########## 输出 ##############
+        # a + b = 3
+
+
 if __name__ == "__main__":
     # handle_file()
 
