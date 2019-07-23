32b20fc769bdf085a741884a0598770efd65422a
michael <potflesh@gmail.com>
2019-07-23 10:48:43 +0800

    feat(python_digital_coding_and_processing): encoding_and_decoding_base64_data()

--- python_digital_coding_and_processing.py
+++ python_digital_coding_and_processing.py
@@ -228,7 +228,27 @@ def encoding_and_decoding_hexadecimal_numbers():
     print(binascii.a2b_hex(h))
 
 
+# Question4: Encoding and decoding Base64 data
+def encoding_and_decoding_base64_data():
+    # Some byte data
+    s = b'hello'
+    import base64
+    # Encode as Base64
+    a = base64.b64encode(s)
+    print(a)
+
+    # Decode from Base64
+    print(base64.b64decode(a))
+
+    # Base64编码仅仅用于面向字节的数据比如字节字符串和字节数组。 此外，编码处理的输出结果
+    # 总是一个字节字符串。 如果你想混合使用Base64编码的数据和Unicode文本，你必须添加一个
+    # 额外的解码步骤。例如：
+    a = base64.b64encode(s).decode('ascii')
+    print(a)
+
+
 if __name__ == '__main__':
     # read_and_write_csv_data()
     # read_and_write_json_data()
-    encoding_and_decoding_hexadecimal_numbers()
+    # encoding_and_decoding_hexadecimal_numbers()
+    encoding_and_decoding_base64_data()
